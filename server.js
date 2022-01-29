eca338c3ad6cb97f91906b42ee8c4822a88b3223
Wai Lon <rxdvd0@gmail.com>
2022-01-29 16:41:34 +0000

    boiler plate

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
+app.use(express.json());
+/*
+app.get('/posts', (req, res) => {
+    
+});
 
+app.get('/post/:pid', (req, res) => {
 
+});
+
+app.post('/posts', (req, res) => {
+
+});
+
+app.post('/post/:pid/comment/:cid/emoji', (req, res) => {
+
+});
+
+app.post('/post/:pid/emoji', (req, res) => {
+
+});
+*/
 module.exports = app;
