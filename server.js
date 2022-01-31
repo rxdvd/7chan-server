35649e0e43c383ae68717cb8db016a9d0bb1d10d
Sukhraj Bhandal <sukhrajbhandal1998@gmail.com>
2022-01-31 12:22:31 +0000

    added R from CRUD

--- server.js
+++ server.js
@@ -7,14 +7,25 @@ app.use(express.json());
 
 // C R U D
 
+app.get('/', (req, res) => {
+    res.json('Testing Testing 1...2...3')
+})
+
 // READ ALL
 app.get('/posts', (req, res) => {
-    
+    res.json(posts)
 });
 
 // READ by ID
 app.get('/post/:pid', (req, res) => {
-
+    try {
+        let requestedPost = req.params.pid
+        let matchingPost = search.find((item) => item.id === requestedPost);
+        if(!matchingPost) { throw new Error(`Sorry we don't have a post id of ${requestedSearch}`)}
+        res.json(matchingPost)
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
 });
 
 // CREATE - one post
@@ -29,7 +40,7 @@ app.post('/posts', (req, res) => {
 
 // CREATE emoji
 app.post('/post/:pid/emoji', (req, res) => {
-
+    
 });
 
 module.exports = app;
