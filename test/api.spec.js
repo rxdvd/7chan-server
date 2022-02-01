5a4f78982bc899ccd502a93d63986f4ea2db13d6
Sukhraj Bhandal <sukhrajbhandal1998@gmail.com>
2022-02-01 16:11:36 +0000

    testing get functions completed, post incomplete, patch incomplete

--- test/api.spec.js
+++ test/api.spec.js
@@ -7,77 +7,77 @@ describe('posts api', () => {
 
     // For GET requests
 
-    test('responds GET/ with Welcome to all code runners!', done => {
-        request(app)
-            .get('/')
-            .expect(200, done)
-    })
-    test('responds GET/posts with all the posts correct', done => {
-        request(app)
-            .get('/posts')
-            .expect(200, done)
-    })
+    // test('responds GET/ with Welcome to all code runners!', done => {
+    //     request(app)
+    //         .get('/')
+    //         .expect(200, done)
+    // })
+    // test('responds GET/posts with all the posts correct', done => {
+    //     request(app)
+    //         .get('/posts')
+    //         .expect(200, done)
+    // })
 
-    test('responds GET/posts  with all the post in a JSON file', done => {
-        request(app)
-            .get('/posts/')
-            .expect('content-Type', 'application/json; charset=utf-8', done)
+    // test('responds GET/posts  with all the post in a JSON file', done => {
+    //     request(app)
+    //         .get('/posts/')
+    //         .expect('content-Type', 'application/json; charset=utf-8', done)
 
-    })
+    // })
 
-    test('responds GET/posts/:pid with with a a id=1 correct', done => {
-        request(app)
-            .get('/posts')
-            .expect(200, done)
-    })
+    // test('responds GET/posts/:pid with with a a id=1 correct', done => {
+    //     request(app)
+    //         .get('/posts')
+    //         .expect(200, done)
+    // })
 
-    test('responds GET/posts/:pid with with a JSON file', done => {
-        request(app)
-            .get('/posts/1')
-            .expect('content-Type', 'application/json; charset=utf-8', done)
-    })
+    // test('responds GET/posts/:pid with with a JSON file', done => {
+    //     request(app)
+    //         .get('/posts/1')
+    //         .expect('content-Type', 'application/json; charset=utf-8', done)
+    // })
 
     // For POST for making a new post
 
-    test('to see if it responds correctly and with a JSON file', done => {
-        request(app)
-            .get('/posts')
-            .expect('Content-Type', 'application/json; charset=utf-8')
-            .expect(200, done)
-    })
-
-    // test('to test if the new post', done => {
-    //     let testNewPost = {
-    //                     "pid": 3,
-    //                     "title": "test text",
-    //                     "message": "test message",
-    //                     "giphy": "",
-    //                     "comments": [  ],
-    //                     "reactions": {
-    //                         "thumbs_up": ["1234","2345","3456"],
-    //                         "thumbs_down": ["1234","2345","3456","4567","3477"],
-    //                         "heart": []
-    //                     },
-    //                      "timestamp": Date.now
-    //                     }
-                        
+    // test('to see if it responds correctly and with a JSON file', done => {
     //     request(app)
-    //         .post('/posts')
-    //         .send(testNewPost)
-    //         .expect(201, {                        
-    //             "pid": 3,
-    //             "title": "test text",
-    //             "message": "test message",
-    //             "giphy": "",
-    //             "comments": [  ],
-    //             "reactions": {
-    //                 "thumbs_up": ["1234","2345","3456"],
-    //                 "thumbs_down": ["1234","2345","3456","4567","3477"],
-    //                 "heart": []
-    //         },
-    //          "timestamp": Date.now}, done)
-
+    //         .get('/posts')
+    //         .expect('Content-Type', 'application/json; charset=utf-8')
+    //         .expect(200, done)
     // })
+
+    test('to test if the new post', done => {
+        let testNewPost = {
+                        "pid": 3,
+                        "title": "test text",
+                        "message": "test message",
+                        "giphy": "",
+                        "comments": [  ],
+                        "reactions": {
+                            "thumbs_up": ["1234","2345","3456"],
+                            "thumbs_down": ["1234","2345","3456","4567","3477"],
+                            "heart": []
+                        },
+                         "timestamp": Date.now
+                        }
+                        
+        request(app)
+            .post('/posts')
+            .send(testNewPost)
+            .expect(201, {                        
+                "pid": 3,
+                "title": "test text",
+                "message": "test message",
+                "giphy": "",
+                "comments": [  ],
+                "reactions": {
+                    "thumbs_up": ["1234","2345","3456"],
+                    "thumbs_down": ["1234","2345","3456","4567","3477"],
+                    "heart": []
+            },
+             "timestamp": Date.now}, done)
+
+    })
 
 
     // For POST for making a new comment
