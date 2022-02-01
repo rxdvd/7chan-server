82c26c2b778feb4e09d6d59cdaebdcf65774fd3f
giacomo <88339527+gi-ba-bu@users.noreply.github.com>
2022-02-01 16:09:52 +0100

    added user specific routes

--- server.js
+++ server.js
@@ -2,131 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const fs = require("fs")
-
 app.use(cors());
 app.use(express.json());
 
-const posts = require("./data/posts.json")
-
-// C R U D
+const router1 = require('./routes/userRoutes'); // user specific routes
+app.use('/posts', router1); // add user routes when the "/post" route is requested
 
 // Welcome message
 app.get('/', (req, res) => {
     res.json('Welcome to all code runners!')
 })
 
-// READ all
-app.get('/posts', (req, res) => {
-    res.json(posts)
-});
-
-// READ by pid
-app.get('/posts/:pid', (req, res) => {
-    try {
-        let requestedPostId = req.params.pid;
-        let matchingPost = posts.find( ({pid}) => pid == requestedPostId);
-        if(!matchingPost) { throw new Error(`Sorry we don't have a post id of ${requestedPostId}`)}
-        res.json(matchingPost);
-    } catch (error) {
-        res.status(404).json({message: error.message});
-    }
-    
-});
-
-// CREATE - one post -will need to add item persistently
-// CLIENT INPUT LOOKS LIKE THE FOLLOWING (can be used to try the route)
-// { 
-//     "title": "new post, new title",
-//     "message": "new post, new message",
-//     "giphy": "https://media0.giphy.com/media/enNk8yXXpDHYzD3tqF/giphy.gif?cid=5c2583098j8m18kl0gh9xq3wv558ovtq7d8ykr71wna9ycwg&rid=giphy.gif&ct=g"
-// }
-
-app.post('/posts', (req, res) =>{
-    try{
-    let newId = posts[0].pid + 1;
-    let newTitle = req.body.title;
-    let newMessage = req.body.message;
-    let newGiphy = req.body.giphy;
-    let timeNow = Date.now();
-    if(!newTitle) { throw new Error(`No title entered.`)};
-    if(!newMessage) { throw new Error(`No text entered.`)};
-    let newPost = {
-        "pid": newId,
-        "title": newTitle,
-        "message": newMessage,
-        "giphy": newGiphy,
-        "comments": [],
-        "reactions": {
-            "thumbs_up": [],
-            "thumbs_down": [],
-            "heart": []
-        },
-        "timestamp": timeNow
-    }
-    posts.unshift(newPost);
-    console.log(typeof posts)
-    //fs.writeFileSync("./data/posts.json", JSON.stringify(posts));
-    res.json(newPost); // only one res. will go through
-    //res.send("comment added successfully"); // only one res. will go through
-    } catch (error) {
-        res.status(400).json({message: error.message});
-    }
-});
-
-// CREATE - add one comment 
-
-// CLIENT INPUT LOOKS LIKE THE FOLLOWING (can be used to try the route)::
-// { 
-//  "comment": "another comment"
-// }
-
-// WILL NEED TO FILL IN AN ELEMEMENT THAT LOOKS LIKE THE FOLLOWING:
-// "comments": [
-//     {
-//         "cid": 0,
-//         "comment": "message",
-//         "timestamp": 1285253453498
-//     }
-// ]
-
-app.post('/posts/:pid/comments', (req, res) => {
-    try {
-        let newMessage = req.body.comment;
-        let timeNow = Date.now();
-        let requestedPostId = req.params.pid;
-        let postIndex = posts.findIndex(x => x.pid == requestedPostId);
-        if(postIndex === -1) { throw new Error(`Sorry we can't find a post id of ${requestedPostId}`)};
-        let matchingPost = posts[postIndex];
-        let newcId = 0;
-        if(matchingPost.comments[0]) {newcId = matchingPost.comments[0].cid+1};
-        let newComment = {"cid": newcId, "comment": newMessage, "timestamp":timeNow};
-        posts[postIndex].comments.unshift(newComment);
-        //fs.writeFileSync("./data/posts.json", JSON.stringify(posts));
-        res.json(posts[postIndex]); // only one res. will go through
-        //res.send("comment added successfully") // only one res. will go through
-      } catch (error) {
-        res.status(404).json({message: error.message}); 
-    }
-});
-
-// CREATE emoji {"emoji": "thumbs_down", "uid": "1234"}
-app.patch('/posts/:pid/emoji', (req, res) => {
-
-    let userId = req.body.uid; 
-    let requestedEmoji = req.body.emoji;
-    let requestedPostId = req.params.pid;
-
-    let postIndex = posts.findIndex(x => x.pid == requestedPostId);  // find post
-    let isUIDin = posts[postIndex].reactions[requestedEmoji].includes(userId);  // check if uid from request is in the array
-   
-    if(!isUIDin) //toggle "function"
-        {posts[postIndex].reactions[requestedEmoji].unshift(userId);}
-       else
-        {posts[postIndex].reactions[requestedEmoji].splice(posts[postIndex].reactions[requestedEmoji].indexOf(userId), 1);} 
-    //fs.writeFileSync("./data/posts.json", JSON.stringify(posts));
-    res.json(posts[postIndex]);
-    
-});
 
 module.exports = app;
