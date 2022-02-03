590a36efff97a4f249ee141198cfedc9e21dd940
Wai Lon <rxdvd0@gmail.com>
2022-02-03 17:28:28 +0000

    add tags

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -36,6 +36,7 @@ router1.post('/', (req, res) =>{
     let newTitle = req.body.title;
     let newMessage = req.body.message;
     let newGiphy = req.body.giphy;
+    let newTags = req.body.tags;
     let timeNow = Date.now();
     if(!newTitle) { throw new Error(`No title entered.`)};
     if(!newMessage) { throw new Error(`No text entered.`)};
@@ -50,6 +51,7 @@ router1.post('/', (req, res) =>{
             "thumbs_down": [],
             "heart": []
         },
+        "tags": newTags,
         "timestamp": timeNow
     }
     posts.unshift(newPost);
