1dbbbc39828b2e37481b0ae0eb9d3ac12e9f5b92
Wai Lon <rxdvd0@gmail.com>
2022-02-11 09:34:22 +0000

    use data from mongodb

--- models/post.js
+++ models/post.js
@@ -1,4 +1,4 @@
-const postData = require("../data/posts.json");
+const { init } = require("../mongo")
 
 class Post {
     constructor(data){
@@ -8,9 +8,11 @@ class Post {
     }
 
     static get all() {
-        return new Promise((resolve, reject) => {
+        return new Promise( async (resolve, reject) => {
             try {
-                const posts = postData.map(post => new Post(post));
+                const db = await init();
+                const postsData = await db.collection('posts').find().toArray();
+                const posts = postsData.map(post => new Post(post));
                 resolve(posts);
             } catch(err) {
                 reject(err);
@@ -19,12 +21,17 @@ class Post {
     }
 
     static byID(pid) {
-        return new Promise((resolve, reject) => {
+        return new Promise( async (resolve, reject) => {
             try {
                 if(!pid) throw new Error("No post id specified.");
-                let matchingPost = postData.find(post => post.pid == pid);
-                if(!matchingPost) throw new Error("Post not found.");
-                resolve(new Post(matchingPost));
+
+                const db = await init();
+                const postsData = await db.collection('posts').find({
+                    pid: parseInt(pid)
+                }).toArray();
+
+                if(!postsData[0]) throw new Error("Post not found.");
+                resolve(new Post(postsData[0]));
             } catch(err) {
                 reject(err);
             }
@@ -32,12 +39,15 @@ class Post {
     }
 
     static newPost(title, message, giphy="", tags=[]) {
-        return new Promise((resolve, reject) => {
+        return new Promise( async (resolve, reject) => {
             try {
                 if(!title) throw new Error("No title.");
                 if(!message) throw new Error("No message.");
-                let maxId = Math.max(...postData.map(post => post.pid));
-                let newId = postData.length ? maxId + 1 : 0;
+
+                const db = await init();
+                const sortedPosts = await db.collection('posts').find().sort({pid:-1}).toArray();
+
+                let newId = sortedPosts.length ? sortedPosts[0].pid + 1 : 0;
                 let newPost = {
                     "pid": newId,
                     "title": title,
@@ -52,8 +62,10 @@ class Post {
                     "tags": tags,
                     "timestamp": Date.now()
                 };
-                postData.unshift(new Post(newPost));
-                resolve(newPost);
+
+                const postsData = await db.collection('posts').insertOne(newPost);
+
+                resolve(new Post({_id: postsData.insertedId, ...newPost}));
             } catch(err) {
                 reject(err);
             }
@@ -61,7 +73,7 @@ class Post {
     }
 
     newComment(text){
-        return new Promise((resolve, reject) => {
+        return new Promise( async (resolve, reject) => {
             try {
                 if(!text) throw new Error("No comment.");
                 let maxCid = Math.max(...this.comments.map(comment => comment.cid));
@@ -72,6 +84,14 @@ class Post {
                     "timestamp": Date.now()
                 };
                 this.comments.unshift(newComment);
+
+                const db = await init();
+                await db.collection('posts').findOneAndUpdate({
+                    pid: parseInt(this.pid)
+                },{
+                    $set: { comments: this.comments }
+                });
+
                 resolve(this);
             } catch(err){
                 reject(err);
@@ -80,7 +100,7 @@ class Post {
     }
 
     toggleReaction(emoji, uid){
-        return new Promise((resolve, reject) => {
+        return new Promise( async (resolve, reject) => {
             try {
                 if(!emoji) throw new Error("No reaction specified.");
                 if(!uid) throw new Error("No UID specified.");
@@ -91,6 +111,14 @@ class Post {
                 } else {
                     reactions.splice(uidIndex, 1);
                 }
+
+                const db = await init();
+                await db.collection('posts').findOneAndUpdate({
+                    pid: parseInt(this.pid)
+                },{
+                    $set: { reactions: this.reactions }
+                });
+
                 resolve(this);
             } catch(err){
                 reject(err);
